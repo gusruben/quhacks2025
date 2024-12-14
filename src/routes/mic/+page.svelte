8c9a054cb5bfa29318b8df7c3404f5b817affe5b
Archonic <gabrielc2904@gmail.com>
2024-12-14 14:30:55 -0500

    Add basic recording

--- src/routes/mic/+page.svelte
+++ src/routes/mic/+page.svelte
@@ -1,8 +1,43 @@
-<script>
 
+<script>
+ // @ts-ignore
+    export const CLIP_LEN = 60; //multiplied by MS_INTERVAL
+    export const MS_INTERVAL = 1000;
+    import { onMount } from 'svelte'
+    let media = [];
+    //debug text
+    let text = "Recording";
+    let mediaRecorder = null;
+    function askPermission(){
+        navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => {
+            // Initialize MediaRecorder
+            mediaRecorder = new MediaRecorder(stream, {mimeType: "audio/webm"});
+            mediaRecorder.ondataavailable = function(e){
+                media.push(e.data);
+                if(media.length > CLIP_LEN){
+                    //delete first element
+                    media.shift();
+                }
+                text = media.length.toString();
+            }
+            mediaRecorder.start(MS_INTERVAL);
+        });
+    }
+    onMount(() => {
+        askPermission();
+    });
 </script>
 
-<div id="mic-page-vertical-flex" class="absolute top">
+<div id="body-flex" class="flex flex-col gap-8 justify-center w-full h-full">
+
+    <button id="mic-icon" class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full w-48 h-48 bg-orange-400">
+        
+    </button>
+    <div id="debug" class="text-2xl text-center">
+        {text}
+    </div>
+
     
 </div>
 
